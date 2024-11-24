d4e7aa796b6025218ec16ee9e408f409e578b269
Gamal Khaled <gamal.khaled11@gmail.com>
2024-11-25 01:26:20 +0200

    adding theme and spacing

---
+++ src/theme/theme.ts
@@ -0,0 +1,19 @@
+import Theme from "../types/Theme";
+
+const theme: Theme = {
+    primary: '#191926',
+    background: '#20202f',
+    card: '#242638',
+    disabled: '#3c3c3c',
+
+    font: "#fff",
+    fontSecondary: "#252537",
+
+    danger: "#F32013",
+    warning: "#F0D500",
+    success: "#0CBE24",
+
+    border: '#666980',
+};
+
+export default theme;
---
+++ src/types/Theme.ts
@@ -0,0 +1,15 @@
+export default interface Theme {
+    primary: string;
+    background: string;
+    card: string;
+    disabled: string;
+
+    font: string;
+    fontSecondary: string;
+
+    danger: string;
+    warning: string;
+    success: string;
+
+    border: string;
+}
